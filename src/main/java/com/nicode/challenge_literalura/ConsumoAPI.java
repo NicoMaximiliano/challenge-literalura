5d997b1c6fa354052d0f8b3b6704cf03d3c09403
NicoMaximiliano <nicolas.orellano25@outlook.com.ar>
2025-12-16 23:39:34 -0300

    Agregando la clase ConsumoAPI para obtener datos de la API

---
+++ src/main/java/com/nicode/challenge_literalura/ConsumoAPI.java
@@ -0,0 +1,30 @@
+package com.nicode.challenge_literalura;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsumoAPI {
+    private static final HttpClient client = HttpClient.newHttpClient();
+    private static final String URL_BASE = "https://gutendex.com";
+
+
+    public static String obtenerDatos(String urlEndpoint) {
+        String urlCompleta = URL_BASE + urlEndpoint;
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(urlCompleta))
+                .build();
+
+        HttpResponse<String> response;
+
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return response.body();
+    }
+}
